688248e71c1c6ce778d630adbfd8a75e77cbf6c9
ddtamn <disanderlidsbun@gmail.com>
2023-03-15 19:11:26 +0800

    step 1

--- src/routes/event/[slug]/+page.svelte
+++ src/routes/event/[slug]/+page.svelte
@@ -4,96 +4,90 @@
   import { onMount } from "svelte";
   export let data;
 
-  const ffmpeg = createFFmpeg({
-    log: true,
-    corePath: "http://localhost:5173/ffmpeg-core.js",
-  });
-
   onMount(async () => {
-    await ffmpeg.load().then(() => {
-      proccess("051B3DD0-F41C-400C-810D-371DABF41239.mov");
-    });
+    // await ffmpeg.load().then(() => {
+    //   proccess("051B3DD0-F41C-400C-810D-371DABF41239.mov");
+    // });
   });
 
   async function proccess(/** @type {any} */ file) {
-    console.log(file);
-
+    const ffmpeg = createFFmpeg({
+      progress: (e) => {
+        console.log(e.ratio);
+      },
+      log: true,
+      corePath: "http://localhost:5173/ffmpeg-core.js",
+    });
     let storageURL =
       "https://yzxsiqwrmigznhixkeyy.supabase.co/storage/v1/object/public/videos";
-    ffmpeg.FS(
-      "writeFile",
-      "video.mp4",
-      await fetchFile(`${storageURL}/${file}`)
-    );
 
-    await ffmpeg.run(
-      "-i",
-      "video.mp4",
-      "-ss",
-      "00:00",
-      "-to",
-      "00:03",
-      "-c:v",
-      "libx264",
-      "-crf",
-      "30",
-      "result1.mp4"
-    );
+    await ffmpeg.load().then(async () => {
+      ffmpeg.FS(
+        "writeFile",
+        "video.mp4",
+        await fetchFile(`${storageURL}/${file}`)
+      );
 
-    await ffmpeg.run(
-      "-i",
-      "video.mp4",
-      "-ss",
-      "00:05",
-      "-to",
-      "00:08",
-      "-c:v",
-      "libx264",
-      "-crf",
-      "30",
-      "slow2.mp4"
-    );
+      await ffmpeg.run(
+        "-i",
+        "video.mp4",
+        "-ss",
+        "00:00",
+        "-to",
+        "00:03",
+        "-c:v",
+        "libx264",
+        "-crf",
+        "30",
+        "result1.mp4"
+      );
 
-    // await ffmpeg.run("-i", "slow2.mp4", "-vf", "setpts=2*PTS", "result2.mp4");
+      await ffmpeg.run("-i", "result1.mp4", "-c", "copy", "intermediate1.ts");
 
-    await ffmpeg.run(
-      "-i",
-      "result1.mp4",
-      "-i",
-      "slow2.mp4",
-      "-filter_complex",
-      "[0:0][0:1][1:0][1:1]concat=n=2:v=1:a=1[outv][outa]",
-      "-map",
-      "[outv]",
-      "-map",
-      "[outa]",
-      "result.mp4"
-    );
+      await ffmpeg.run(
+        "-i",
+        "video.mp4",
+        "-ss",
+        "00:03",
+        "-to",
+        "00:06",
+        "-c:v",
+        "libx264",
+        "-crf",
+        "30",
+        "result2.mp4"
+      );
 
-    // await ffmpeg.run(
-    //   "-i",
-    //   "result1.mp4",
-    //   "-i",
-    //   "result2slow.mp4",
-    //   "-i",
-    //   "result3.mp4",
-    //   "-i",
-    //   "result3revese.mp4",
-    //   "-i",
-    //   "result2slowrevervse.mp4",
-    //   "-i",
-    //   "result1reverse.mp4",
-    //   "-filter_complex",
-    //   "[0:v][0:a][1:v][1:a][2:v][2:a][3:v][3:a][4:v][4:a][5:v][5:a]concat=n=6:v=1:a=1",
-    //   "-vsync",
-    //   "vfr",
-    //   "result.mp4"
-    // );
-    const result = await ffmpeg.FS("readFile", "result.mp4");
-    const url = URL.createObjectURL(
-      new Blob([result.buffer], { type: "video/mp4" })
-    );
-    console.log(url);
+      await ffmpeg.run(
+        "-i",
+        "result2.mp4",
+        "-filter:v",
+        "setpts=2.0*PTS",
+        "result2slow.mp4"
+      );
+
+      await ffmpeg.run(
+        "-i",
+        "result2slow.mp4",
+        "-c",
+        "copy",
+        "intermediate2.ts"
+      );
+
+      await ffmpeg.run(
+        "-i",
+        "concat:intermediate1.ts|intermediate2.ts",
+        "-c",
+        "copy",
+        "output.mp4"
+      );
+
+      const result = await ffmpeg.FS("readFile", "output.mp4");
+      const url = URL.createObjectURL(
+        new Blob([result.buffer], { type: "video/mp4" })
+      );
+      console.log(url);
+    });
   }
 
   supabase
@@ -102,10 +96,30 @@
       proccess(data.payload.file_path);
     })
     .subscribe();
+
+  function record() {
+    const channel = supabase.channel("record").subscribe((status) => {
+      if (status === "SUBSCRIBED") {
+        channel.send({
+          type: "broadcast",
+          event: "supa",
+          payload: { org: "supabase" },
+        });
+      }
+    });
+  }
 </script>
 
 <div class="p-4 py-8">
   <div class="text-center">
     <h1 class="font-bold text-lg">{data.event.title}</h1>
+  </div>
+  <div class="text-center mt-5">
+    <button
+      on:click={() => record()}
+      type="button"
+      class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5"
+      >Record</button
+    >
   </div>
 </div>
